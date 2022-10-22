9ea96ba0313e40ebd9a2746d7386295ec09d10ca
Maria <mtens17@gmail.com>
2022-10-22 12:30:59 +0100

    Change to item 0c

--- examples/0c-independent-loop-scopes.js
+++ examples/0c-independent-loop-scopes.js
@@ -5,12 +5,12 @@ console.log("Start of programme");
 const healthyFoods = ['fruit', 'veg'];
 const junkFoods = ['chocolate', 'cake', 'poison'];
 
-for (let healthyItem of healthyFoods) {
-  console.log(`${healthyItem} is normally considered a healthy food`);
+for (let item of healthyFoods) {
+  console.log(`${item} is normally considered a healthy food`);
 }
 
-for (let junkItem of junkFoods) {
-  console.log(`${junkItem} is NOT normally considered a healthy food`);
+for (let item of junkFoods) {
+  console.log(`${item} is NOT normally considered a healthy food`);
   // below will error. Why? Because healthyItem was defined in the previous loop scope
   //console.log(`...so, swap it for ${healthyItem}!`);
 }
