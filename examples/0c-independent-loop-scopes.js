35b188457855d3296c823acc6c57049757b4753b
Maria <mtens17@gmail.com>
2022-10-22 12:25:08 +0100

    Comment out error 0c

--- examples/0c-independent-loop-scopes.js
+++ examples/0c-independent-loop-scopes.js
@@ -11,8 +11,8 @@ for (let healthyItem of healthyFoods) {
 
 for (let junkItem of junkFoods) {
   console.log(`${junkItem} is NOT normally considered a healthy food`);
-  // below will error. Why?
-  console.log(`...so, swap it for ${healthyItem}!`);
+  // below will error. Why? Because healthyItem was defined in the previous loop scope
+  //console.log(`...so, swap it for ${healthyItem}!`);
 }
 
 console.log("Reached the end of my programme!");
